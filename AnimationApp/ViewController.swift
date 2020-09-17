33f59d4212fbe90ec3fdcdf6d423025261e5552b
VGO <vgorobets@yandex.ru>
2020-09-17 10:25:10 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by VICTOR on 17.09.2020.
+//  Copyright © 2020 VICTOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
